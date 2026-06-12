916a2f8e8f79302b26f19022297ac2b3171e0d7e
Joey Ye <joeyye@Joeys-MacBook-Pro.local>
2026-06-11 21:22:17 -0700

    make swagger more descriptive and grouping by routes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.api.polls import router
 
-app = FastAPI(title="Polls API")
+app = FastAPI(
+    title="Polls API",
+    description="A simple API to create and vote on polls",
+    version="0.1",
+    openapi_tags=[
+        {
+            "name": "polls",
+            "description": "Operations related to creating and viewing polls"
+        }
+    ]
+    )
 
 app.include_router(router, prefix="/polls", tags=["polls"])
 
@@ -11,6 +21,3 @@ def health():
     return {"status": "ok"}
 
 
-@app.get("/test")
-def test():
-    return {"message": "hello there"}
